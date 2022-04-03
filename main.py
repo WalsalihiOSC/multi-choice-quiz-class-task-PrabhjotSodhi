8e998af380d8924ae16f4b8ce948f8199b21d1b3
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-04-03 19:37:52 +1200

    check radio button answers

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class MultiChoiceClass:
         self.selection = tk.IntVar(frame)
         self.selection.set(1)
         self.options = {"Vladivostok": 1,"Astana": 2,"Ulan Bator": 3,"Lhasa": 4}
+    def check_answer(self, selection, label):
+        if selection.get() == 3:
+            label['text']="Correct!"
+            print("Correct")
+            print(selection.get())
+        else:
+            label['text']="Incorrect!"
+            print("Incorrect")
+            print(selection.get())
     def get_options(self):
         return self.options.items()
     def get_selection(self):
@@ -21,4 +30,5 @@ if __name__ == '__main__':
     main_frame = tk.Frame(root)
     main_frame.pack()
     backend = MultiChoiceClass(main_frame)
+    MultiChoiceGUI(main_frame).display()
     root.mainloop()
