ef6120bfa955e9eaa1f3ed7d2fefeee239b44d42
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-04-03 19:35:16 +1200

    Tkinter Window Init

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+        
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('500x300')
+    root.title("Multi Choice Quiz")
+    main_frame = tk.Frame(root)
+    main_frame.pack()
+    root.mainloop()
